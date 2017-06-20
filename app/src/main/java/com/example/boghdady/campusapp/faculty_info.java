dcdbbddb057dcb4ee5d89b0f87977134a8a6a5bc
spongebob <shimaa.naga7@gmail.com>
2017-06-20 23:53:16 +0200

    faculty info layout

---
+++ app/src/main/java/com/example/boghdady/campusapp/faculty_info.java
@@ -0,0 +1,15 @@
+package com.example.boghdady.campusapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class faculty_info extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty_info);
+    }
+}
